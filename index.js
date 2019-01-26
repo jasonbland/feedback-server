64725d6b0eb1ed345e439583e2dbb06b27aa183c
Jason Bland <jtbland@gmail.com>
2019-01-25 22:06:55 -0800

    followup deployment

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({
+    name: {
+      first: 'Jason',
+      last: 'Bland'
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5000;
