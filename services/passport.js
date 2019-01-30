3132257811a1b50f2f62a0474f38b08d6d88554a
Jason Bland <jtbland@gmail.com>
2019-01-29 22:53:07 -0800

    remove references to keys file

--- services/passport.js
+++ services/passport.js
@@ -1,13 +1,12 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
 
 // use google strategy for passport
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_ID || keys.googleClientID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || keys.googleClientSecret,
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
