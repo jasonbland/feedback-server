f1cf03daa6f1d413c082468a3c34eb69d04716f4
Jason Bland <jtbland@gmail.com>
2019-02-02 23:14:32 -0800

    add passport serialize for user session cookie

--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,16 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 // use google strategy for passport
 passport.use(
   new GoogleStrategy(
