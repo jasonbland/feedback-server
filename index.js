104f6a0cdb6993de352e416a49041bcfaa191ab5
Jason Bland <jtbland@gmail.com>
2019-02-02 23:30:47 -0800

    add authentication flow

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 require('./models/User');
 require('./services/passport');
@@ -7,6 +9,16 @@ require('./services/passport');
 mongoose.connect(process.env.PROD_MONGODB);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [process.env.COOKIE_KEY]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
