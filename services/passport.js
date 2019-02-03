e932a0199134f8f8946b57f4f833363112d29f0d
Jason Bland <jtbland@gmail.com>
2019-02-02 21:33:27 -0800

    check user before adding to mongo

--- services/passport.js
+++ services/passport.js
@@ -13,11 +13,13 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      console.log(refreshToken);
-      console.log(profile);
-
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // already have a record
+        } else {
+          new User({ googleId: profile.id }).save();
+        }
+      });
     }
   )
 );
