e50ec27bf0273e0d933add6e6a84aa0436e9623e
Jason Bland <jtbland@gmail.com>
2019-02-03 23:04:01 -0800

    add logout route handler

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -16,6 +16,11 @@ module.exports = app => {
   // code returned (get user profile)
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
