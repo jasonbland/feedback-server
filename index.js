86446435aadd1539d61a5e3402b962a7211d9c1e
Jason Bland <jtbland@gmail.com>
2019-02-04 21:16:00 -0800

    add dev and prod keys configuration

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const keys = require('./config/keys');
 
 require('./models/User');
 require('./services/passport');
 
-mongoose.connect(process.env.PROD_MONGODB);
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [process.env.COOKIE_KEY]
+    keys: [keys.cookieKey]
   })
 );
 
--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
 const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
@@ -18,8 +19,8 @@ passport.deserializeUser((id, done) => {
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientID: keys.googleClientId,
+      clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
