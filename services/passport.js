331548105b817964022f1bcce4fb3129669da311
Jason Bland <jtbland@gmail.com>
2019-02-06 22:46:23 -0800

    update password strategy with async await

--- services/passport.js
+++ services/passport.js
@@ -24,15 +24,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // already have a record
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id }).save().then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
