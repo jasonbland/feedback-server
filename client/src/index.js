0d5853732ef0fcb1c07a3ba5a33968f8c90154a1
Jason Bland <jtbland@gmail.com>
2019-02-09 19:46:10 -0800

    add redux thunk dependency and middleware

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
 import 'materialize-css/dist/css/materialize.min.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
