15b6d949af033ccc80494797cf2634b7619138c0
Jason Bland <jtbland@gmail.com>
2019-02-10 21:32:40 -0800

    add render content helper

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,23 +2,42 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class Header extends React.Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Google Login</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">Feedback</a>
-          <ul className="right">
-            <li>
-              <a>Google Login</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
+const mapStateToProps = ({ auth }) => {
+  return { auth };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   null
 )(Header);
