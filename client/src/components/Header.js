4283b3f5d3fa2a5f43f908fa84d65bc45a4b02b1
Jason Bland <jtbland@gmail.com>
2019-02-10 21:50:40 -0800

    add logout redirect

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -15,7 +15,7 @@ class Header extends React.Component {
       default:
         return (
           <li>
-            <a href="">Logout</a>
+            <a href="/api/logout">Logout</a>
           </li>
         );
     }
