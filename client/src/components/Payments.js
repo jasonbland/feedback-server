8daf779077d7f4b666aa669ad2309f5c4d2fa65d
Jason Bland <jtbland@gmail.com>
2019-02-11 21:53:22 -0800

    add stripe payments component to header

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -3,7 +3,9 @@ import StripeCheckout from 'react-stripe-checkout';
 
 class Payments extends Component {
   render() {
-    return <StripeCheckout />;
+    return (
+      <StripeCheckout amount={500} token={token => console.log(token)} stripeKey={process.env.REACT_APP_STRIPE_KEY} />
+    );
   }
 }
 
