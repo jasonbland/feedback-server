2459c3bfcb98f5550d208a204d3068d150f8e3b8
Jason Bland <jtbland@gmail.com>
2019-02-13 22:42:27 -0800

    hook up handletoken to payment

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Payments extends Component {
   render() {
@@ -8,7 +10,7 @@ class Payments extends Component {
         name="Feedback"
         description="$5 for 5 email credits"
         amount={500}
-        token={token => console.log(token)}
+        token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
         <button className="waves-effect waves-light btn blue">Add Credits</button>
@@ -17,4 +19,7 @@ class Payments extends Component {
   }
 }
 
-export default Payments;
+export default connect(
+  null,
+  actions
+)(Payments);
