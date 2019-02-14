86502eb36a1725daf70baf94155603070f447b4e
Jason Bland <jtbland@gmail.com>
2019-02-13 22:02:10 -0800

    add name and desc to payment button

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -4,7 +4,15 @@ import StripeCheckout from 'react-stripe-checkout';
 class Payments extends Component {
   render() {
     return (
-      <StripeCheckout amount={500} token={token => console.log(token)} stripeKey={process.env.REACT_APP_STRIPE_KEY} />
+      <StripeCheckout
+        name="Feedback"
+        description="$5 for 5 email credits"
+        amount={500}
+        token={token => console.log(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="waves-effect waves-light btn blue">Add Credits</button>
+      </StripeCheckout>
     );
   }
 }
