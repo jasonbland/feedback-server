2a179b00a953496cbd8e411363cf7ac6042f04b7
Jason Bland <jtbland@gmail.com>
2019-02-14 23:52:54 -0800

    remove root auth route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,6 @@
 const passport = require('passport');
 
 module.exports = app => {
-  app.get('/', (req, res) => {
-    res.send('Feedback Server');
-  });
-
   // attempt authentication (get code)
   app.get(
     '/auth/google',
