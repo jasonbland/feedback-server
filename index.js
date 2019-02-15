6a68d50a642196b58d179feb70692cb9dea9d82d
Jason Bland <jtbland@gmail.com>
2019-02-14 23:46:38 -0800

    fix type in path resolve

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'production') {
   // serve up index.html if doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'clent', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
