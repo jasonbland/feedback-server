849016f931d2f1b2c0df4cc167458563e83c94ff
Jason Bland <jtbland@gmail.com>
2019-02-24 22:02:28 -0800

    add recipients user and datesent to new survey

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -11,7 +11,10 @@ module.exports = app => {
     const survey = new Survey({
       title,
       subject,
-      body
+      body,
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
     });
   });
 };
