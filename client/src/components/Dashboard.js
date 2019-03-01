90b3279f5de6508f0a453d06aeeacf25a6d2747f
Jason Bland <jtbland@gmail.com>
2019-02-28 22:03:10 -0800

    add dashboard create button and material icon

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,7 +5,12 @@ class Dashboard extends React.Component {
   render() {
     return (
       <div>
-        <div>Dashboard</div>
+        Dashboard
+        <div className="fixed-action-btn">
+          <a href="/" className="btn-floating btn-large red">
+            <i className="material-icons">add</i>
+          </a>
+        </div>
       </div>
     );
   }
