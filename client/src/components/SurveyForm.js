3aaa47545a1f6765b2daca67c3825a696a5e95e3
Jason Bland <jtbland@gmail.com>
2019-03-05 08:21:01 -0800

    add validate emails logic

--- client/src/components/SurveyForm.js
+++ client/src/components/SurveyForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../utils/validateEmails';
 
 const FIELDS = [
   { label: 'Survey Title', name: 'title' },
@@ -36,6 +37,8 @@ class SurveyForm extends React.Component {
 
 function validate(values) {
   const errors = {};
+
+  errors.emails = validateEmails(values.emails || '');
 
   FIELDS.forEach(({ name }) => {
     if (!values[name]) {
