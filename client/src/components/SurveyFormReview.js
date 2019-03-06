91b70bdbf916780b2402b7ce7e96e19a24510d5c
Jason Bland <jtbland@gmail.com>
2019-03-05 23:41:30 -0800

    add fields to survery form review

--- client/src/components/SurveyFormReview.js
+++ client/src/components/SurveyFormReview.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields';
 
 const SurveyFormReview = ({ formValues, onCancel }) => {
+  const reviewFields = formFields.map(({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
-      <div>
-        <div>
-          <label>Survey Title</label>
-          <div>{formValues.title}</div>
-        </div>
-      </div>
+      {reviewFields}
       <button className="yellow darken-3 btn-flat" onClick={onCancel}>
         Back
       </button>
